58672fe9722b0910c8730591e87f6c7177386cdb
preethi-nk <89142587+preethi-nk@users.noreply.github.com>
2021-12-27 13:10:16 +0530

    Add files via upload

---
+++ Set2/3.sql
@@ -0,0 +1,11 @@
+CREATE PROC set2_3_Sale @ID INT
+AS
+SELEcT s.SalesPersonID,n.FirstName,MAX(o.OrderQty) as OrderQuantity,o.ProductID 
+FROM AdventureWorks2019.Sales.SalesPerson AS p
+INNER JOIN AdventureWorks2019.Sales.SalesOrderHeader AS s on p.BusinessEntityID = s.SalesPersonID 
+INNER JOIN AdventureWorks2019.Person.Person AS n ON p.BusinessEntityID = n.BusinessEntityID
+INNER JOIN AdventureWorks2019.Sales.SalesOrderDetail o on s.SalesOrderID = o.SalesOrderID
+WHERE ProductID = @ID
+GROUP BY  p.BusinessEntityID,s.SalesPersonID,o.ProductID,n.FirstName 
+ORDER BY OrderQuantity desc
+GO
